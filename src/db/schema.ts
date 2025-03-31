c2c65e550beb388a1b893e5bc79a432eb102e227
aju-alen <alenaju1304@gmail.com>
2025-04-01 00:59:29 +0400

    drizzle schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,5 +1,5 @@
-import { pgTable } from "drizzle-orm/pg-core";
+import { pgTable, uuid } from "drizzle-orm/pg-core";
 
 export const users = pgTable("users",{
-
+    id: uuid("id").primaryKey().defaultRandom()
 })
